8b0859aa0089c84388b5f03d7978946c876866cd
Bea Esguerra <bea.esguerra@outlook.com>
2019-03-04 21:58:04 -0700

    adding version and social media icons

--- src/View/Footer.elm
+++ src/View/Footer.elm
@@ -6,6 +6,8 @@ import Html.Styled.Attributes as Attr exposing (..)
 import Model exposing (Model, Route(..))
 import Message exposing (Msg)
 import Style.Theme as Theme
+import View.Svg as Svg
+import Svg.Styled.Attributes as SvgAttr
 
 view : Model -> Html Msg
 view model =
@@ -14,11 +16,44 @@ view model =
             [ displayFlex
             , flexDirection row
             , alignItems center
+            , justifyContent spaceBetween
             , Css.height (px 60)
             , Css.width (pct 100)
             , backgroundColor (Theme.primary model.cache)
             ]
         ]
         [
-            text "hi"
+            footerLeft model,
+            footerRight model
+        ]
+
+footerLeft : Model -> Html Msg
+footerLeft model =
+    div
+        [ css
+            [ margin (px 10)
+            ]
+        ]
+        [
+            text model.cache.version
+        ]
+
+footerRight : Model -> Html Msg
+footerRight model =
+    div
+        [ css
+            [ margin (px 10)
+            ]
+        ]
+        [
+            Svg.github [
+                SvgAttr.css [
+                    margin (px 5)
+                ]
+            ],
+            Svg.linkedin [
+                SvgAttr.css [
+                    margin (px 5)
+                ]
+            ]
         ]
