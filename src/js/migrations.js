da2dd26dbf3f5ae78ce505667e1a665f9442acca
Parasrah <parasrah@tutanota.com>
2019-07-16 17:16:14 -0600

    fix #81

--- src/js/migrations.js
+++ src/js/migrations.js
@@ -45,6 +45,7 @@ createMigration('0.0.26', '0.0.27')
 
 createMigration('0.0.27', '0.0.28')
 createMigration('0.0.28', '0.0.29')
+createMigration('0.0.29', '0.0.30')
 
 /********************************/
 /*            Helpers           */
